66da279ef9918cae15a5299827cde6c05a4006e5
Matt Kouba <matt.kouba@gmail.com>
2023-02-15 23:43:44 -0500

    Update pipeline.sh
    Add ability to specify port with command line arg and to check to make sure it is actually provided

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] #make sure there is at least one arguement.  Should add more checking, but good enoug for now
+then
+          echo "No port provided. Exiting..."
+          exit 1
+fi
+
 while true
 do
 gst-launch-1.0 -v \
@@ -9,5 +15,5 @@ gst-launch-1.0 -v \
         ! nvv4l2decoder \
         ! nvv4l2h265enc control-rate=constant_bitrate \
         ! mpegtsmux \
-        ! srtsink uri="srt://0.0.0.0:9854?mode=listener"
+        ! srtsink uri="srt://0.0.0.0:$1?mode=listener"
 done
