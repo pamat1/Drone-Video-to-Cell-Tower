a9b4303004d4d4ec28716bf6d156e1fe9231846a
pamat1 <58830769+pamat1@users.noreply.github.com>
2023-03-16 13:55:00 -0400

    Added main_rx and main_tx, some bare-bones files that beeline straight to establishing a connection and starting a srt  stream

--- main_rx.py
+++ main_rx.py
@@ -10,6 +10,10 @@ middlemanPort = 9876
 middlemanAddr = (middlemanName, middlemanPort)
 mmSocket = None
 
+HOST = ''
+thisName = None
+thisPort = None
+thisAddr = None
 
 isP2P = False
 p2pAddr = None
@@ -20,6 +24,7 @@ p2pSocket = None
 streamName = None
 streamPort = None
 streamAddr = None
+isStreaming = False
 
 if __name__ == '__main__':
     #Attempt to connect to middleman server
@@ -36,6 +41,22 @@ if __name__ == '__main__':
     print("Connected to server at " + str(middlemanAddr))
     #Once connected, wait for a 'REROUTINGTO' command
     while True:
+        if isP2P:
+            print("Waiting for P2P response...")
+            p2pResponse = p2pSocket.recvfrom(2048)
+            print("received")
+            text = p2pResponse[0].decode()
+            print("P2PResponse[0]: " + text)
+            command = text.split(' ', 1)[1]
+            print("Command: " + command)
+            print("Trimmed response: " + text)
+            if command == 'STREAM_INFO':
+                print("Beginning Streaming... ")
+                streamPort = literal_eval(text)
+                streamAddr = p2pAddr[0] + ":" + str(streamPort)
+                isStreaming = True
+        if isStreaming:
+            break
         if isConnected:
             mmResponse = mmSocket.recvfrom(2048)
             text = mmResponse[0].decode()
@@ -44,16 +65,20 @@ if __name__ == '__main__':
             text = text.split(' ', 1)[1]
             print("Command: " + command)
             print("Trimmed response: " + text)
+            if command == 'INFO_NEW':
+                thisName = literal_eval(text)[0]
+                thisPort = literal_eval(text)[1]
+                thisAddr = (thisName, thisPort)
             if command == 'REROUTINGTO':
                 p2pAddr = literal_eval(text)
                 p2pName = p2pAddr[0]
                 p2pPort = p2pAddr[1]
+                mmSocket.close()
+
                 p2pSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                p2pSocket.bind((thisName, thisPort))
                 print("Rerouting to: " + str(p2pAddr))
+                print("Created new socket for " + str(thisAddr))
                 isP2P = True
-                continue
-            if command == 'STREAM_INFO':
-                streamPort = literal_eval(text)
-                streamAddr = p2pAddr[0] + ":" + str(streamPort)
-                break
+
     subprocess.call([r'receive.bat', streamAddr])
